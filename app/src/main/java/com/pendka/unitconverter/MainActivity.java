c34d0852b5a6acd368d4b2dd38ba77bf4cab60d1
Pepri Andika <pepriandika@gmail.com>
2024-07-04 05:44:14 +0700

    finish project

--- app/src/main/java/com/pendka/unitconverter/MainActivity.java
+++ app/src/main/java/com/pendka/unitconverter/MainActivity.java
@@ -1,6 +1,11 @@
  package com.pendka.unitconverter;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    TextView textView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,25 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputText = editText.getText().toString();
+                double kilos = Double.parseDouble(inputText);
+                double pounds = kilosToPounds(kilos);
+                textView.setText("" + pounds );
+            }
+        });
+    }
+
+    static double kilosToPounds(double kilos){
+        double onePound = 2.20462;
+        return kilos * onePound;
     }
 }
